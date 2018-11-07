cfbf5d89aa2aff1182c631b02aab047b26f67b9d
abdelrahman48 <abdel_rahman_ali@yahoo.com>
2018-11-07 04:42:36 +0200

    add catagory page

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,25 @@
 $(function () {
     $('.carousel').carousel({
         interval: false
-    })
+    });
+
+    $('a').on('click', function (e) { e.preventDefault()} );
+
+    var paginationLi = $('.pagination-list .page-number');
+    
+    paginationLi.on('click', function () {
+        $(this).addClass('active').siblings('.page-number').removeClass('active');
+    });
+    $('.pagination-list .left').on('click', function () {
+        if( paginationLi.filter('.active').index() === 1 ) return;
+        paginationLi.filter('.active').removeClass('active').prev('.page-number').addClass('active');
+    });
+    $('.pagination-list .right').on('click', function () {
+        if( paginationLi.filter('.active').index() === paginationLi.length ) return;
+        paginationLi.filter('.active').removeClass('active').next('.page-number').addClass('active');
+    });
+
+    for(i = 0; i < 2; i++) {
+        $('.category-list .item').clone().insertAfter(".category-list .item:last");
+    }
 });
