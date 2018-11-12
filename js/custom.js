b39322207529340f46ba4fc8fb51438bc3a81117
abdelrahman48 <abdel_rahman_ali@yahoo.com>
2018-11-12 03:07:56 +0200

    add box 3 box modals

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,10 @@ $(function () {
         sizeButton = $('section.product .details .size button'),
         copyNumber = $('section.product .copies-number span'),
         addCopy = $('section.product .copies-number .plus'),
-        removeCopy = $('section.product .copies-number .minus');
+        removeCopy = $('section.product .copies-number .minus'),
+        thumbnailLi = $('section.product .modal-body .thumbnail li'),
+        thumbnailArrowLeft = $('section.product .modal-body .left'),
+        thumbnailArrowright = $('section.product .modal-body .right');
 
     paginationLi.on('click', function () {
         $(this).addClass('active').siblings('.page-number').removeClass('active');
@@ -42,5 +45,14 @@ $(function () {
     removeCopy.on('click', function () {
         if(parseInt(copyNumber.text()) < 1) return;
         copyNumber.text(parseInt(copyNumber.text()) - 1)
-    })
+    });
+
+    thumbnailArrowLeft.on('click', function () {
+        if( thumbnailLi.filter('.active').index() === 0 ) return;
+        thumbnailLi.filter('.active').removeClass('active').prev('li').addClass('active');
+    });
+    thumbnailArrowright.on('click', function () {
+        if( thumbnailLi.filter('.active').index() === thumbnailLi.length - 1 ) return;
+        thumbnailLi.filter('.active').removeClass('active').next('li').addClass('active');
+    });
 });
