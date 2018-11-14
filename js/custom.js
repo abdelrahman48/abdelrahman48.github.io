e2b293d500afbf60f73bb1c5c052e8ee14b75753
abdelrahman48 <abdel_rahman_ali@yahoo.com>
2018-11-14 02:44:08 +0200

    add responsive to copy-11

--- js/custom.js
+++ js/custom.js
@@ -56,3 +56,33 @@ $(function () {
         thumbnailLi.filter('.active').removeClass('active').next('li').addClass('active');
     });
 });
+// Shopping Cart Box Modal Scroll
+var slider = document.querySelector('header .cart-modal .modal-body'),
+    isDown = false,
+    startX,
+    scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;  // stop the fn from running
+    e.preventDefault();
+    var x = e.pageX - slider.offsetLeft,
+        walk = (x - startX) * 3;
+    slider.scrollLeft = scrollLeft - walk;
+});
